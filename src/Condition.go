96999f56b31e11f4ce06a0181807b62479ad28ff
daviejaneway <davie.janeway@btinternet.com>
2012-09-10 23:28:27 +0100

    Added Conditions.go

---
+++ src/Condition.go
@@ -0,0 +1,17 @@
+package C4G
+
+import "fmt"
+
+type Conditional interface {
+  execute(b bool)
+}
+
+type Condition struct {
+  ident string
+}
+
+func (c Condition) execute(b bool) {
+  if ! b {
+    panic(fmt.Sprintf("Condition not met for: '%s'", c.ident))
+  }
+}
