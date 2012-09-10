55b36bb068664ba677b2d1fa033ac3c24ca37b75
U-Davie-PC\Davie <Davie@Davie-PC.(none)>
2012-09-10 22:41:14 +0100

    Added most basic Contractual func

---
+++ examples/basic_contract.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+import "os"
+import "strconv"
+import "github.com/daviejaneway/C4G/src"
+
+func foo(i int) int {
+  j := i * i
+  
+  defer C4G.NewContract("Contract over 'i'", i > 0, j < 100)
+  
+  return j
+}
+
+
+func main() {
+  s := os.Args[1]
+  
+  i, err := strconv.Atoi(s)
+  
+  if err != nil {
+    panic("usage: go contract.go <int>")
+  }
+  
+  fmt.Println(foo(i))
+}
